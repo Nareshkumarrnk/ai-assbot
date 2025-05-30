ddde3ad854052c3874287e392cda9cc5c640e69b
Nareshkumarrnk <nareshkumarit07@gmail.com>
2025-05-30 10:53:57 +0000

    updated handlers

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,33 +1,36 @@
 from pyrogram import Client,filters
 from pyrogram.types import Message
+from pyrogram import enums
 from openai import OpenAI
 from config import OPENAI_KEY
 
-openrouter= OpenAI(
+client = OpenAI(
   base_url="https://openrouter.ai/api/v1",
-  api_key=OPENAI_KEY,
+  api_key= OPENAI_KEY,
 )
 
 def register_handlers(app: Client):
-  @app.on_message(filters.private & filters.text)
-  async def chat_handler(client_tg: Client,message: Message):
-    user_input = message.text
-    await
-message.chat.action.send_chat_action("typing")
-    try:
-      completion = openrouter.chat.completions.create(
+    @app.on_message(filters.private&filters.text)
+    async def chat_handler(client_app, message):
+      user_input=message.text
+      await message.reply_text("Thinking")
+      try:
+        completion = client.chat.completions.create(
   
-  model="openai/gpt-4.1",
-  messages=[
-    {
-      "role": "user",
-      "content":user_input}
-  ]
-      )
-      reply=completion.choices[0].message.content
-    except Exception as e:
-      reply= f"Error:{str(e)}"
-    await message.reply_text(reply)
+        model="mistralai/mistral-7b-instruct",
+        messages=[
+        {
+        "role": "user",
+        "content":user_input
+        }
+        ]
+        )
+        reply=completion.choices[0].message.content
+        await message.reply_text(reply)
+
+      except Exception as e:
+        await message.reply_text("failed to reply")
+        print("error in AI",e)
 
       
 
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from pyrogram import Client
+from pyrogram import Client,enums
 from bot.handlers import register_handlers
 from config import API_ID, API_HASH, BOT_TOKEN
 app = Client("ai_assbot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
