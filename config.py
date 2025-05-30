ca83b53ea86416e8bfa4a6385454c62f79d3dd0c
Nareshkumarrnk <nareshkumarit07@gmail.com>
2025-05-30 12:25:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
-
+import os
+API_ID= int(os.getenv("API_ID"))
+API_HASH= os.getenv("API_HASH")
+BOT_TOKEN= os.getenv("BOT_TOKEN")
+OPENAI_KEY= os.getenv("OPENAI_KEY")
+MONGO_URI= os.getenv("MONGO_URI")
