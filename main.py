b3cd94d1a5b24349b7481bba4fa6ca5e0ab1355d
Nareshkumarrnk <nareshkumarit07@gmail.com>
2025-05-30 14:31:40 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from pyrogram import Client
+from bot.handlers import register_handlers
+from config import API_ID, API_HASH, BOT_TOKEN
+app = Client("ai_assbot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+register_handlers(app)
+print("Bot is running....")
+app.run()
